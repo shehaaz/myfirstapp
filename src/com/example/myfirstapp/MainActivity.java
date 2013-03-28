56efb3bae6eb3dedc828ce72ea9d8688f0bbfbe7
shehaaz <shehaaz@gmail.com>
2013-03-27 20:29:53 -0400

    test

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 
+//test
 public class MainActivity extends Activity {
 
 	 public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
